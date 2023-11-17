78db29baa844642a4cb05a406a406c4bdbe84e13
doom-9 <110001160+doom-9@users.noreply.github.com>
2023-11-17 09:18:31 +0800

    优化代码

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,67 @@
 import { WechatyBuilder } from "wechaty";
-import qrcodeTerminal from "qrcode-terminal";
+import qrCodeTerminal from "qrcode-terminal";
 import config from "./config.js";
 import ChatGPT from "./chatgpt.js";
+import {
+  ContactSelfInterface,
+  MessageInterface,
+  WechatyInterface,
+} from "wechaty/impls";
 
-let bot: any = {};
+let bot: WechatyInterface | null = null;
+
+let userInfo: ContactSelfInterface | null = null;
+
 const startTime = new Date();
+
 let chatGPTClient: any = null;
+
 initProject();
-async function onMessage(msg) {
+
+async function onMessage(msg: MessageInterface) {
   // 避免重复发送
-  if (msg.date() < startTime) {
+  if (msg.date() < startTime || !bot) {
     return;
   }
+  // 消息发送方
   const contact = msg.talker();
-  const receiver = msg.to();
+  // 消息接收方
+  const receiver = msg.listener();
+  // 消息内容
   const content = msg.text().trim();
+  // 消息所在的微信群
   const room = msg.room();
+  // 发送方名称
   const alias = (await contact.alias()) || (await contact.name());
+  // 消息类型
   const isText = msg.type() === bot.Message.Type.Text;
+
+  // 自己发的消息不处理
   if (msg.self()) {
     return;
   }
 
   if (room && isText) {
+    // 群名
     const topic = await room.topic();
-    console.log(
-      `Group name: ${topic} talker: ${await contact.name()} content: ${content}`
-    );
+    // 群里的发言人
+    const name = await contact.name();
 
-    const pattern = RegExp(`^@${receiver.name()}\\s+${config.groupKey}[\\s]*`);
-    if (await msg.mentionSelf()) {
+    console.log(`Group name: ${topic} talker: ${name} content: ${content}`);
+
+    const pattern = RegExp(`^@${userInfo?.name()}\\s+${config.groupKey}[\\s]*`);
+
+    const isMentioned = await msg.mentionSelf();
+
+    if (isMentioned) {
       if (pattern.test(content)) {
         const groupContent = content.replace(pattern, "");
+        console.log(`Group content: ${groupContent}`);
         chatGPTClient.replyMessage(room, groupContent);
         return;
       } else {
         console.log(
-          "Content is not within the scope of the customizition format"
+          "Content is not within the scope of the customization format"
         );
       }
     }
@@ -50,35 +75,37 @@ async function onMessage(msg) {
       chatGPTClient.replyMessage(contact, privateContent);
     } else {
       console.log(
-        "Content is not within the scope of the customizition format"
+        "Content is not within the scope of the customization format"
       );
     }
   }
 }
 
-function onScan(qrcode) {
-  qrcodeTerminal.generate(qrcode, { small: true }); // 在console端显示二维码
-  const qrcodeImageUrl = [
+function onScan(qrCode: string) {
+  qrCodeTerminal.generate(qrCode, { small: true }); // 在console端显示二维码
+  const qrCodeImageUrl = [
     "https://api.qrserver.com/v1/create-qr-code/?data=",
-    encodeURIComponent(qrcode),
+    encodeURIComponent(qrCode),
   ].join("");
 
-  console.log(qrcodeImageUrl);
+  console.log(qrCodeImageUrl);
 }
 
-async function onLogin(user) {
-  console.log(`${user} has logged in`);
+async function onLogin(user: ContactSelfInterface) {
+  userInfo = user;
+  console.log(`${user.name()} has logged in`);
   const date = new Date();
   console.log(`Current time:${date}`);
 }
 
-function onLogout(user) {
-  console.log(`${user} has logged out`);
+function onLogout(user: ContactSelfInterface) {
+  console.log(`${user.name()} has logged out`);
 }
 
 async function initProject() {
   try {
     chatGPTClient = new ChatGPT();
+
     bot = WechatyBuilder.build({
       name: "WechatEveryDay",
       puppet: "wechaty-puppet-wechat", // 如果有token，记得更换对应的puppet
